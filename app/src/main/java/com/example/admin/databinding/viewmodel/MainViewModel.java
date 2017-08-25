bcd1f08fa1963b55c9e0d425871a5567a31aa661
JeongEuiJoon <dordong327@gmail.com>
2017-08-25 16:50:02 +0900

    progress bar 적용 완료

--- app/src/main/java/com/example/admin/databinding/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/admin/databinding/viewmodel/MainViewModel.java
@@ -2,6 +2,7 @@ package com.example.admin.databinding.viewmodel;
 
 import android.arch.lifecycle.ViewModel;
 import android.databinding.ObservableField;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.view.View;
 
@@ -15,5 +16,16 @@ public class MainViewModel extends ViewModel {
     @NonNull
     public ObservableField<Integer> mProgressBarStatus = new ObservableField<>(View.GONE);
 
+    public MainViewModel() {
+        Handler handler = new Handler();
+        mProgressBarStatus.set(View.VISIBLE);
 
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mTitle.set("데이터 로딩 완료");
+                mProgressBarStatus.set(View.GONE);
+            }
+        }, 3000);
+    }
 }
